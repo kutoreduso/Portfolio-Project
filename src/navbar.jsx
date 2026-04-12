a60cf084d1acb4b7113d4807332f6e42a42f0e3b
kutoreduso <kurtcantiga16@gmail.com>
2026-04-12 15:41:22 +0800

    change navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -14,23 +14,23 @@ const Navbar = () => {
             </h1>
             </Link>
                <nav className="font-inter">
-  <ul className="md:flex-row flex-col flex gap-5 font-normal font-inter text-[#E5E5E5]">
+  <ul className="md:flex-row flex-col flex gap-5 font-inter text-[#E5E5E5] font-bold">
     {/* Galleria Link - Works fine */}
     <Link to="/poster">
-      <li className="md:w-25 rounded-2xl border-2 border-[#e5e5e5] text-center bg-[#e5e5e5] text-[#0A0A0A] hover:bg-[#0A0a0a] uppercase hover:text-[#e5e5e5] duration-300">
+      <li className="md:w-25 rounded-2xl  text-center  text-[#e9e9e9]  uppercase ">
         Galleria
       </li>
     </Link>
 
     {/* ABOUT Link - Fixed to use hash if About is a section on the Home page */}
     <Link to="/#about">
-  <li className="md:w-25 rounded-2xl border-2 border-[#e5e5e5] text-center bg-[#e5e5e5] text-[#0A0A0A] hover:bg-[#0A0a0a] hover:text-[#e5e5e5] duration-300">
+  <li className="md:w-25 rounded-2xl  text-center  text-[#e9e9e9] uppercase ">
     ABOUT
   </li>
 </Link>
 
 <Link to="/#contact">
-  <li className="w-30 rounded-2xl border-2 border-[#e5e5e5] text-center bg-[#e5e5e5] text-[#0A0A0A] hover:bg-[#0A0a0a] hover:text-[#e5e5e5] duration-300">
+  <li className="w-30 rounded-2xl  text-center  text-[#e9e9e9]  uppercase">
     CONTACT
   </li>
 </Link>
